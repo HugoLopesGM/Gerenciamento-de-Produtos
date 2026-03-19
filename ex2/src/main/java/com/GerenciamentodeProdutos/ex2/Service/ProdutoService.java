595b98395414295209f6141c464c461a7648dfd1
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2026-03-19 20:33:35 -0300

    Service

---
+++ ex2/src/main/java/com/GerenciamentodeProdutos/ex2/Service/ProdutoService.java
@@ -0,0 +1,34 @@
+package com.GerenciamentodeProdutos.ex2.Service;
+
+
+import com.GerenciamentodeProdutos.ex2.model.Produto;
+import com.GerenciamentodeProdutos.ex2.repository.RepositorioProduto;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+    @Autowired
+    private RepositorioProduto repositorioProduto;
+
+    public Produto criarProduto(Produto produto){
+      return repositorioProduto.save(produto);
+    }
+
+    public List<Produto>ListarTodos(){
+        return repositorioProduto.findAll();
+    }
+
+    public Optional<Produto>buscarProduto(Long id){
+        return repositorioProduto.findById(id);
+    }
+
+    public void deletar(Long id){
+        repositorioProduto.deleteById(id);
+    }
+}
