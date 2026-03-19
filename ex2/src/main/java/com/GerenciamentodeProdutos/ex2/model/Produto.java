a517e353619f2326f0a89705392ac930b30c14e6
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2026-03-19 20:26:27 -0300

    Model

---
+++ ex2/src/main/java/com/GerenciamentodeProdutos/ex2/model/Produto.java
@@ -0,0 +1,53 @@
+package com.GerenciamentodeProdutos.ex2.model;
+
+import jakarta.persistence.*;
+import org.aspectj.apache.bcel.classfile.LineNumber;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_produto")
+    private String nome;
+    @Column(name = "preco_produto")
+    private BigDecimal preco;
+
+    private Integer estoque;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
